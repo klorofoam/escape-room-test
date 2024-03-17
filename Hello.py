645555a7a23723748fc9fcc47cbdb6f12abf10d1
JC L <132242339+klorofoam@users.noreply.github.com>
2024-03-17 12:42:25 +0000

    Try use dictionary to store questions, answers and hints

--- Hello.py
+++ Hello.py
@@ -17,37 +17,92 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
+questions = {1:"Question 1",
+             2:"Question 2",
+             3:"Question 3",
+             4:"Question 4",
+             5:"Question 5",
+             6:"Question 6"}
+
+answers = {1:"Answer 1",
+           2:"Answer 2",
+           3:"Answer 3",
+           4:"Answer 4",
+           5:"Answer 5",
+           6:"Answer 6"}
+
+hint_level_1 = {1:"Hint 1 Level 1",
+                2:"Hint 2 Level 1",
+                3:"Hint 3 Level 1",
+                4:"Hint 4 Level 1",
+                5:"Hint 5 Level 1",
+                6:"Hint 6 Level 1"}
+
+hint_level_2 = {1:"Hint 1 Level 2",
+                2:"Hint 2 Level 2",
+                3:"Hint 3 Level 2",
+                4:"Hint 4 Level 2",
+                5:"Hint 5 Level 2",
+                6:"Hint 6 Level 2"}
+
+if 'progress' not in st.session_state:
+    st.session_state.progress = 0
+
+if 'answers' not in st.session_state:
+    st.session_state.answers = {}
 
 def run():
     st.set_page_config(
         page_title="Hello",
         page_icon="👋",
     )
-    if 'progress' not in st.session_state:
-        st.session_state.progress = 0
+
     st.write("# This is an example of an escape room")
-
-    st.write("Question 1: abcdefg")
-    answer_1 = st.text_input("Enter your answer to question 1")
-    if st.button("Submit answer 1"):
-        if answer_1 == "test":
-            st.write("Correct!")
-            st.session_state.progress = 1
-        else:
-            st.write("Wrong. Hint 1")
-
-    if st.session_state.progress > 0:
-        st.write("Question 2: abcdefg")
-        answer_2 = st.text_input("Enter your answer to question 2")
-        if st.button("Submit answer 2"):
-            if answer_2 == "test123":
-                st.write("Correct!")
-                st.session_state.progress = 2
-            else:
-                st.write("Wrong. Hint 2")
+    if st.button("Start Escape Room"):
+        st.session_state.progress = 1
     
-    if st.session_state.progress > 1:
-        st.write("Congrats! You done")
+    if st.session_state.progress > 6:
+        st.write("Congratulations! You've Escaped!")
+        if st.button("Restart Escape Room"):
+            st.session_state.progress = 1
+    else:
+        st.write(questions[st.session_state.progress])
+        st.session_state.answers[st.session_state.progress] = st.text_input("Enter your answer")
+        if st.button("Submit answer 1"):
+            if st.session_state.answers[st.session_state.progress] == answers[st.session_state.progress]:
+                st.write("Correct!!")
+                if st.button("Next Question"):
+                    st.session_state.progress = st.session_state.progress + 1
+                if st.button("Previous Question"):
+                    st.session_state.progress = st.session_state.progress - 1
+        with st.expander("Hint Level 1"):
+            st.write(hint_level_1[st.session_state.progress])
+        with st.expander("Hint Level 2"):
+            st.write(hint_level_2[st.session_state.progress])
+
+
+
+    # st.write("Question 1: abcdefg")
+    # answer_1 = st.text_input("Enter your answer to question 1")
+    # if st.button("Submit answer 1"):
+    #     if answer_1 == "test":
+    #         st.write("Correct!")
+    #         st.session_state.progress = 1
+    #     else:
+    #         st.write("Wrong. Hint 1")
+
+    # if st.session_state.progress > 0:
+    #     st.write("Question 2: abcdefg")
+    #     answer_2 = st.text_input("Enter your answer to question 2")
+    #     if st.button("Submit answer 2"):
+    #         if answer_2 == "test123":
+    #             st.write("Correct!")
+    #             st.session_state.progress = 2
+    #         else:
+    #             st.write("Wrong. Hint 2")
+    
+    # if st.session_state.progress > 1:
+    #     st.write("Congrats! You done")
 
 
 if __name__ == "__main__":
